7b4e3890debdf4e2f899b1ea25f6f97a250f5937
Mayank Gupta <52287975+Mayank-7@users.noreply.github.com>
2020-08-02 17:34:31 +0530

    Update post.php
    Added up function named create to insert.

--- core/post.php
+++ core/post.php
@@ -29,9 +29,35 @@
 		//execute query
 		$stmt->execute();
 
-		return $stmt;
-
-		
+		return $stmt;	
 	}
+		public function create(){
+			//create query
+			$query = 'INSERT INTO ' . $this->table . ' SET catid = :catid, catname= :catname, description = :description, status= :status';
+			//prepare statemenet
+			$stmt = $this->conn->prepare($query);
+			//clean data
+			$this->catid = htmlspecialchars(strip_tags($this->catid));
+			$this->catname = htmlspecialchars(strip_tags($this->catname));
+			$this->description = htmlspecialchars(strip_tags($this->description));
+			$this->status = htmlspecialchars(strip_tags($this->status));
+			///binding of parameters
+
+			$stmt->bindParam('catid', $this->catid);
+			$stmt->bindParam('catname', $this->catname);	
+			$stmt->bindParam('description', $this->description);
+			$stmt->bindParam('status', $this->status);
+
+			//execute query
+			if($stmt->execute()){
+				return true;
+			}
+
+			//print error if something goes wrong
+			printf("Error %s. \n", $stmt->error);
+			return false;
+
+
+		}
 }
 ?>
